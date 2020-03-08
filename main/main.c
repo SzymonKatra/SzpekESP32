8eb4139d939d70de28bfddc29810f7c7c792bca5
Szymon Katra <szymon.katra@gmail.com>
2020-03-08 22:32:52 +0100

    pms3003 test

--- main/main.c
+++ main/main.c
@@ -11,10 +11,41 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "pmsX003.h"
+#include <driver/uart.h>
+#include "driver/gpio.h"
 
+void smogSensorTask()
+{
+	while (1)
+	{
+		pmsX003Data_t result;
+		pmsX003Result_t errcode = pmsX003_read(&result);
+		printf("%d, PM1: %d, PM2.5: %d, PM10: %d\n", errcode, result.pm1Outdoor, result.pm2_5Outdoor, result.pm10Outdoor);
+		vTaskDelay(100 / portTICK_PERIOD_MS);
+	}
+}
 
 void app_main()
 {
+	uart_config_t uartPms3003Config = {
+	        .baud_rate = 9600,
+	        .data_bits = UART_DATA_8_BITS,
+	        .parity = UART_PARITY_DISABLE,
+	        .stop_bits = UART_STOP_BITS_1,
+	        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+	    };
+
+	uart_param_config(UART_NUM_2, &uartPms3003Config);
+	uart_set_pin(UART_NUM_2, UART_PIN_NO_CHANGE, GPIO_NUM_16, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+	uart_driver_install(UART_NUM_2, 128 * 2, 0, 0, NULL, 0);
+
+	pmsX003_init(PMS3003, UART_NUM_2, 0, PMSX003_MODE_ACTIVE);
+
+	xTaskCreate(&smogSensorTask, "task", 2048, NULL, 3, NULL);
+
+
+
     printf("Hello world!\n");
 
     /* Print chip information */
@@ -36,5 +67,5 @@ void app_main()
     }
     printf("Restarting now.\n");
     fflush(stdout);
-    esp_restart();
+    //esp_restart();
 }
