6644cb1f87fa36e2ea359d8dc98255dccdf37049
Szymon Katra <szymon.katra@gmail.com>
2020-05-14 02:21:24 +0200

    Capative site for configuration. HTTP requests auto-redirect

--- main/configServer.c
+++ main/configServer.c
@@ -12,30 +12,55 @@
 #include "settings.h"
 #include "logUtils.h"
 #include "mathUtils.h"
+#include "captdns.h"
 
 static const char* LOG_TAG = "ConfigServer";
 
 static const char* INDEX_CONTENT =
-	"<html><body>"
-	"Konfiguruj:"
-	"<form action=\"config\" method=\"post\">"
-	"SSID: <input type=\"text\" name=\"ssid\"><br>"
-	"Haslo: <input type=\"text\" name=\"password\"><br>"
-	"<input type=\"submit\" value=\"Zapisz\">"
+	"<html>"
+	"<head>"
+	"<meta charset=\"UTF-8\">"
+	"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
+	"</head>"
+	"<body>"
+	"Skonfiguruj swój sensor Szpek:<br>"
+	"Configure your Szpek sensor:<br>"
+	"<form action=\"config\" method=\"post\" style=\"margin-top: 1rem;\">"
+	"<div style=\"margin-top: 1rem;\">Nazwa sieci (SSID):<br><input type=\"text\" name=\"ssid\"></div>"
+	"<div style=\"margin-top: 1rem;\">Hasło (Password):<br><input type=\"text\" name=\"password\"></div>"
+	"<input style=\"margin-top: 1rem; font-size: 1.1rem\" type=\"submit\" value=\"Zapisz (Save)\">"
 	"</form>"
-	"</body></html>";
+	"</body>"
+	"</html>";
 
 static const char* CONFIG_CONTENT =
-	"<html><body>"
-	"Konfiguracja zapisana pomyslnie!<br>"
-	"Teraz przytrzymaj przycisk az zgasnie czerwona dioda.<br>"
-	"Urzadzenie rozpocznie laczenie sie z WiFi i zostanie to zasygnalizowane zapaleniem sie zielonej diody."
-	"</body></html>";
+	"<html>"
+	"<head>"
+	"<meta charset=\"UTF-8\">"
+	"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
+	"</head>"
+	"<body>"
+	"<div>"
+	"Konfiguracja zapisana pomyślnie!<br>"
+	"Teraz przytrzymaj przycisk na urządzeniu aż zgaśnie czerwona dioda.<br>"
+	"Urządzenie rozpocznie łaczenie się ze skonfigurowaną siecią WiFi.<br>"
+	"Pomyślne połączenie zostanie zasygnalizowane zapaleniem zielonej diody.<br>"
+	"</div>"
+	"<div style=\"margin-top: 1rem;\">"
+	"Configuration saved successfully!<br>"
+	"Now hold the button until the red LED goes out.<br>"
+	"The device will begin connecting to configured WiFi.<br>"
+	"Successfull connection will be indicated bythe greed LED lighting up.<br>"
+	"</div>"
+	"</body>"
+	"</html>";
 
 static httpd_handle_t s_serverHandle;
 static esp_err_t rootGETHandler(httpd_req_t* request);
 static esp_err_t configGETHandler(httpd_req_t* request);
 static esp_err_t configPOSTHandler(httpd_req_t* request);
+
+static esp_err_t notFoundHandler(httpd_req_t *req, httpd_err_code_t error);
 
 static bool getFormValue(const char* buffer, const char* key, char* value, size_t len);
 
@@ -57,11 +82,14 @@ static const httpd_uri_t s_configPOST = {
 	.handler   = configPOSTHandler
 };
 
+static bool s_captDnsRunning = false;
+
 bool configServerStart()
 {
 	LOG_INFO("Starting...");
 
 	httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+	config.lru_purge_enable = true;
 	esp_err_t err = httpd_start(&s_serverHandle, &config);
 	if (err != ESP_OK)
 	{
@@ -72,8 +100,16 @@ bool configServerStart()
 	httpd_register_uri_handler(s_serverHandle, &s_rootGET);
 	httpd_register_uri_handler(s_serverHandle, &s_configGET);
 	httpd_register_uri_handler(s_serverHandle, &s_configPOST);
+	httpd_register_err_handler(s_serverHandle, HTTPD_404_NOT_FOUND, notFoundHandler);
 
 	LOG_INFO("Started!");
+
+	if (!s_captDnsRunning)
+	{
+		LOG_INFO("Starting captdns...");
+		captdnsInit();
+		s_captDnsRunning = true;
+	}
 
 	return true;
 }
@@ -99,7 +135,7 @@ static esp_err_t rootGETHandler(httpd_req_t* request)
 static esp_err_t configGETHandler(httpd_req_t* request)
 {
 	httpd_resp_set_status(request, "302 Found");
-	httpd_resp_set_hdr(request, "Location", "/");
+	httpd_resp_set_hdr(request, "Location", "http://szpek.local");
 	httpd_resp_send(request, NULL, 0);
 
 	return ESP_OK;
@@ -124,6 +160,16 @@ static esp_err_t configPOSTHandler(httpd_req_t* request)
 	httpd_resp_send(request, CONFIG_CONTENT, strlen(CONFIG_CONTENT));
 
     return ESP_OK;
+}
+
+static esp_err_t notFoundHandler(httpd_req_t* request, httpd_err_code_t error)
+{
+	LOG_INFO("Redirecting to main page");
+	httpd_resp_set_status(request, "302 Found");
+	httpd_resp_set_hdr(request, "Location", "http://szpek.local");
+	httpd_resp_send(request, NULL, 0);
+
+	return ESP_OK;
 }
 
 static bool getFormValue(const char* buffer, const char* key, char* value, size_t len)
