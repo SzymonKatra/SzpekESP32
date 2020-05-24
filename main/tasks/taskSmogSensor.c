85a57e6ad2df5f7269d13ae8718c9ce6f6580252
Szymon Katra <szymon.katra@gmail.com>
2020-05-24 12:40:06 +0200

    PMS test, some releases

--- main/tasks/taskSmogSensor.c
+++ main/tasks/taskSmogSensor.c
@@ -13,11 +13,15 @@
 
 static int readFromUart(uint8_t* buffer, size_t len);
 
+static void pmsTest();
+
 void taskSmogSensor(void* p)
 {
 	pmsParams_t params;
 	params.model = PMS_7003;
 	params.dataReadFun = &readFromUart;
+
+	pmsTest(&params);
 
 	while (1)
 	{
@@ -38,4 +42,34 @@ void taskSmogSensor(void* p)
 static int readFromUart(uint8_t* buffer, size_t len)
 {
 	return uart_read_bytes(APP_PMS_UART_PORT, buffer, len, 2000 / portTICK_PERIOD_MS);
+}
+
+static void pmsTest(const pmsParams_t* params)
+{
+	pmsData_t result;
+	pmsError_t err = PMS_ERR_DATA_READ;
+	while (err == PMS_ERR_DATA_READ)
+	{
+		err = pmsRead(params, &result);
+		if (err == PMS_ERR_DATA_READ)
+		{
+			LOG_TASK_ERROR("Cannot read bytes from PMS7003!");
+		}
+	}
+
+	while (err != PMS_OK)
+	{
+		for (size_t i = 0; i < 10; i ++)
+		{
+			err = pmsRead(params, &result);
+			if (err == PMS_OK) break;
+		}
+
+		if (err != PMS_OK)
+		{
+			LOG_TASK_ERROR("Receiving invalid data from PMS7003, last error %d!", err);
+		}
+	}
+
+	LOG_TASK_INFO("PMS7003 first read success! PM1: %d, PM2.5: %d, PM10: %d", result.pm1Outdoor, result.pm2_5Outdoor, result.pm10Outdoor);
 }
